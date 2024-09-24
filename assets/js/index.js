71825bcf02110859d43e5c132cb2de76fcb3cf07
abolfazl <abolfazles@gmail.com>
2024-09-24 22:05:43 +0330

    update menue

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,10 @@ let profileContent = document.querySelector('.my-dropdown-content')
 let aboutdropdown = document.querySelector('.dropdown-links-one')
 let smMenueBtn = document.getElementById('sm-menue-btn')
 let smMenue = document.querySelector('.sm-manue')
+let amlakMenu = document.getElementById('menu-amlak')
+let Vehicles = document.getElementById('Vehicles')
+let boxMenu1 = document.querySelector('.about-link-menue1')
+let boxMenu2 = document.querySelector('.about-link-menue2')
 
 function addClasList () {
     dropdowncCntent.classList.toggle('dis-block')
@@ -15,7 +19,15 @@ function myProfile () {
 function HambergerMenue () {
     smMenue.classList.toggle('dis-grid')
 }
+function openBoxHandeler1 () {
+    boxMenu1.classList.toggle('dis-block')
+}
+function openBoxHandeler2 () {
+    boxMenu2.classList.toggle('dis-block')
+}
 
 btnNav.addEventListener('click', addClasList)
 profileBtn.addEventListener('click', myProfile)
 smMenueBtn.addEventListener('click', HambergerMenue)
+amlakMenu.addEventListener('click', openBoxHandeler1)
+Vehicles.addEventListener('click', openBoxHandeler2)
