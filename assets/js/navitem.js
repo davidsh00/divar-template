1efffd4b7644ad252222171258ab0f7bc483fb30
abolfazl <abolfazles@gmail.com>
2024-09-29 03:00:14 +0330

    done project

---
+++ assets/js/navitem.js
@@ -0,0 +1,152 @@
+const listItems1 = document.querySelector('.list-item1');
+const listItems2 = document.querySelector('.list-item2');
+const listItems3 = document.querySelector('.list-item3');
+const listItems4 = document.querySelector('.list-item4');
+const listItems5 = document.querySelector('.list-item5');
+const listItems6 = document.querySelector('.list-item6');
+const listItems7 = document.querySelector('.list-item7');
+const listItems8 = document.querySelector('.list-item8');
+const listItems9 = document.querySelector('.list-item9');
+const listItems10 = document.querySelector('.list-item10');
+const linkNav1 = document.querySelector('.link-navbar-menu1');
+const linkNav2 = document.querySelector('.link-navbar-menu2');
+const linkNav3 = document.querySelector('.link-navbar-menu3');
+const linkNav4 = document.querySelector('.link-navbar-menu4');
+const linkNav5 = document.querySelector('.link-navbar-menu5');
+const linkNav6 = document.querySelector('.link-navbar-menu6');
+const linkNav7 = document.querySelector('.link-navbar-menu7');
+const linkNav8 = document.querySelector('.link-navbar-menu8');
+const linkNav9 = document.querySelector('.link-navbar-menu9');
+const linkNav10 = document.querySelector('.link-navbar-menu10');
+
+function mouseHandeler1 () {
+    linkNav1.style.display = 'grid'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+
+}
+function mouseHandeler2 () {
+    linkNav2.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler3 () {
+    linkNav3.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler4 () {
+    linkNav4.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler5 () {
+    linkNav5.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler6 () {
+    linkNav6.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler7 () {
+    linkNav7.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler8 () {
+    linkNav8.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav9.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler9 () {
+    linkNav9.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav10.style.display = 'none'
+}
+function mouseHandeler10 () {
+    linkNav10.style.display = 'grid'
+    linkNav1.style.display = 'none'
+    linkNav2.style.display = 'none'
+    linkNav3.style.display = 'none'
+    linkNav4.style.display = 'none'
+    linkNav5.style.display = 'none'
+    linkNav6.style.display = 'none'
+    linkNav7.style.display = 'none'
+    linkNav8.style.display = 'none'
+    linkNav9.style.display = 'none'
+}
+listItems1.addEventListener('mousemove', mouseHandeler1)
+listItems2.addEventListener('mousemove', mouseHandeler2)
+listItems3.addEventListener('mousemove', mouseHandeler3)
+listItems4.addEventListener('mousemove', mouseHandeler4)
+listItems5.addEventListener('mousemove', mouseHandeler5)
+listItems6.addEventListener('mousemove', mouseHandeler6)
+listItems7.addEventListener('mousemove', mouseHandeler7)
+listItems8.addEventListener('mousemove', mouseHandeler8)
+listItems9.addEventListener('mousemove', mouseHandeler9)
+listItems10.addEventListener('mousemove',mouseHandeler10)
